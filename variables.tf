97d200df0227337871f5384444768858152061c6
Aayush Sharma <aayusharma1401@gmail.com>
2023-07-26 02:02:45 +0530

    feat: add compute instance in OCI

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "compartment_id" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "private_key_path" {}
+variable "fingerprint" {}
+variable "source_id" {}
+variable "subnet_id" {}
